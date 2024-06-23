ed0cbf4c35bc6e12e384bb8191f21b24f87c3e99
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-06-23 18:31:21 +0800

    Implement recursive route name generation

--- apps/web/src/routes/routes.ts
+++ apps/web/src/routes/routes.ts
@@ -1,25 +1,39 @@
 import { RouteObject } from "react-router";
 
+const ApplicationWrapper = () =>
+  import("../components/common/ApplicationWrapper");
 const Dashboard = () => import("../pages/Dashboard");
 const Login = () => import("../pages/Login");
 const NotFound = () => import("../pages/NotFound");
 
-export const routes = [
-  { path: "/", lazy: Dashboard },
-  { path: "/login", lazy: Login },
-  { path: "/not_found", lazy: NotFound },
-] as const satisfies RouteObject[];
+export const routes: RouteObject[] = [
+  {
+    path: "/",
+    lazy: ApplicationWrapper,
+    children: [
+      { path: "", lazy: Dashboard },
+      { path: "login", lazy: Login },
+      { path: "not_found", lazy: NotFound },
+    ],
+  },
+];
 
 type RouteKeys = Uppercase<"Dashboard" | "Login" | "NotFound">;
-export const RouteNames = Object.freeze(
-  routes.reduce(
+const buildRouteNames = (routes: RouteObject[], prefix = "") => {
+  return routes.reduce(
     (acc, route) => {
       const key = route.lazy?.name?.toUpperCase();
-      if (key) {
-        acc[key as RouteKeys] = route.path;
+      if (route.children) {
+        acc = {
+          ...acc,
+          ...buildRouteNames(route.children, prefix + route.path),
+        };
+      } else if (key) {
+        acc[key as RouteKeys] = route.path!;
       }
       return acc;
     },
     {} as Record<RouteKeys, string>
-  )
-);
+  );
+};
+export const RouteNames = Object.freeze(buildRouteNames(routes));
