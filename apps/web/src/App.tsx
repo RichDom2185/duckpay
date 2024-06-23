afdd82de194e4a8e2fe8007fb99c0bc46867b3bb
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-06-23 18:31:56 +0800

    Use new react router splat path logic

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -2,7 +2,10 @@ import { RouterProvider } from "react-router";
 import { createBrowserRouter } from "react-router-dom";
 import { routes } from "./routes/routes";
 
-const router = createBrowserRouter(routes);
+const router = createBrowserRouter(routes, {
+  future: { v7_relativeSplatPath: true },
+});
+
 const App: React.FC = () => {
   return <RouterProvider router={router} />;
 };
