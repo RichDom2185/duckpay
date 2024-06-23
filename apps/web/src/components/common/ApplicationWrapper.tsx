a85eb3de5aa84316bd32b36d32d17af08ac31441
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-06-23 18:28:42 +0800

    Create application wrapper

---
+++ apps/web/src/components/common/ApplicationWrapper.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Outlet } from "react-router";
+import NavBar from "./NavBar";
+
+const ApplicationWrapper: React.FC = () => {
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+    </>
+  );
+};
+
+export const Component = ApplicationWrapper;
+Component.displayName = "ApplicationWrapper";
+
+export default ApplicationWrapper;
