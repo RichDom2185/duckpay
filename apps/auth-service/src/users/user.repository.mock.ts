fc7495cbf83ada36d05b0901535e1138ae33b1ac
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-07-01 18:58:48 +0800

    Create mock User repository
    Also refactored and exported some classes.

---
+++ apps/auth-service/src/users/user.repository.mock.ts
@@ -0,0 +1,14 @@
+import { User } from "@repo/database";
+import { IUserRepository } from "./user.repository";
+
+export class MockUserRepository implements IUserRepository {
+  async findUserById(id: string): Promise<User | null> {
+    const fakeUser: User = {
+      id,
+      name: `User ${id}`,
+      email: `${id}${id}@example.com`,
+      emailVerified: new Date(),
+    };
+    return fakeUser;
+  }
+}
--- apps/auth-service/src/users/user.repository.ts
+++ apps/auth-service/src/users/user.repository.ts
@@ -4,13 +4,10 @@ export interface IUserRepository {
   findUserById(id: string): Promise<User | null>;
 }
 
-class UserRepository {
-  private client: PrismaClient;
-  constructor(client: PrismaClient) {
-    this.client = client;
-  }
+export class UserRepository implements IUserRepository {
+  constructor(private db: PrismaClient) {}
 
   async findUserById(id: string): Promise<User | null> {
-    return this.client.user.findUnique({ where: { id } });
+    return this.db.user.findUnique({ where: { id } });
   }
 }
