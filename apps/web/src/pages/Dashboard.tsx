87347a17c7c0b71084172a2bd5e8182c86118a7a
Tan Jing Sheng <tanjs41200@gmail.com>
2024-07-03 23:51:34 +0800

    Jingsheng/draggable (#8)
    * update dist
    
    * install react dnd
    
    * add token skeleton

--- apps/web/src/pages/Dashboard.tsx
+++ apps/web/src/pages/Dashboard.tsx
@@ -1,5 +1,30 @@
+import Token from "../components/Token";
+
+const tokens = [
+  { id: 1, amount: 10 },
+  { id: 2, amount: 20 },
+  { id: 3, amount: 30 },
+  { id: 4, amount: 40 },
+  { id: 5, amount: 50 },
+  { id: 6, amount: 60 },
+  { id: 7, amount: 70 },
+  { id: 8, amount: 80 },
+  { id: 9, amount: 90 },
+  { id: 10, amount: 100 },
+];
+
 const Dashboard: React.FC = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="flex flex-wrap justify-center items-center m-4">
+      {tokens.map((token) => (
+        <Token
+          key={token.id.toString()}
+          tokenId={token.id.toString()}
+          tokenValue={token.amount.toString()}
+        />
+      ))}
+    </div>
+  );
 };
 
 export const Component = Dashboard;
