7dd054bd62cf30d8d9414e8ec5e1c658ebf1e62f
Tan Jing Sheng <tanjs41200@gmail.com>
2024-07-07 00:08:15 +0800

    Create transaction repo (#32)
    * add withdraw route
    
    * remove debug message
    
    ---------
    
    Co-authored-by: Richard Dominick <34370238+RichDom2185@users.noreply.github.com>

---
+++ apps/payment-service/src/transaction/transaction.repository.ts
@@ -0,0 +1,17 @@
+import { PrismaClient, Transaction } from "@repo/payments";
+
+export interface ITransactionRepository {
+  create(
+    transaction: Pick<Transaction, "amount" | "tokenId" | "currency">,
+  ): Promise<Transaction | null>;
+}
+
+export default class TransactionRepository implements ITransactionRepository {
+  constructor(private db: PrismaClient) {}
+
+  async create(
+    transaction: Pick<Transaction, "amount" | "tokenId" | "currency">,
+  ): Promise<Transaction | null> {
+    return this.db.transaction.create({ data: transaction });
+  }
+};
