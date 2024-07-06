e44e279c9fa94fa49bbd8438f34a5e0b3e91eeab
Wen Li <79928462+wendy0107@users.noreply.github.com>
2024-07-06 15:13:54 +0800

    Wenli/types (#11)
    * qr code package
    
    * add react-drag-and-drop

---
+++ apps/web/src/types/react-drag-and-drop.d.ts
@@ -0,0 +1,5 @@
+// src/types/react-drag-and-drop.d.ts
+declare module 'react-drag-and-drop' {
+    export class Draggable extends React.Component<any, any> {}
+    export class Droppable extends React.Component<any, any> {}
+}
