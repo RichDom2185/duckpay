920736ac4d28d4c0979b655cadf66e2598fe7a87
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-07-06 17:18:41 +0800

    Simplify FE (misc changes) 1 (#20)
    * Reformat code
    
    * Remove unnecessary menu code
    
    * Install iconfify
    
    * Fix menu positioning and add sticky menu
    
    * Use iconify icons for brevity
    
    * Simplify modal

--- apps/web/src/components/QrCodeGenerator.tsx
+++ apps/web/src/components/QrCodeGenerator.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import duck from '../assets/images/duck-transparent-bg.png';
-import { CustomQRCode } from 'custom-qr-code/react';
+import { CustomQRCode } from "custom-qr-code/react";
+import React from "react";
+import duck from "../assets/images/duck-transparent-bg.png";
 
 interface QrCodeGeneratorProps {
-  data: string;  // QR data passed as a prop
+  data: string; // QR data passed as a prop
 }
 
 const QrCodeGenerator: React.FC<QrCodeGeneratorProps> = ({ data }) => {
@@ -19,24 +19,23 @@ const QrCodeGenerator: React.FC<QrCodeGeneratorProps> = ({ data }) => {
         data={data}
         image={duck}
         dotsOptions={{
-            color: "#4267B2",
-            gradient: {
-              type: "radial",
-              colorStops: [{ offset: 0, color: '#1E00FF' }, { offset: 5, color: '#00FFFF' }]
-            },
-            type: "extra-rounded",
-          }}
-        cornersSquareOptions={
-            {
-                color: "#00BFFF",
-                type: "extra-rounded",
-            }
-        }
-        cornersDotOptions={
-            {
-                color: "#ffffff",
-            }
-        }
+          color: "#4267B2",
+          gradient: {
+            type: "radial",
+            colorStops: [
+              { offset: 0, color: "#1E00FF" },
+              { offset: 5, color: "#00FFFF" },
+            ],
+          },
+          type: "extra-rounded",
+        }}
+        cornersSquareOptions={{
+          color: "#00BFFF",
+          type: "extra-rounded",
+        }}
+        cornersDotOptions={{
+          color: "#ffffff",
+        }}
         backgroundOptions={{
           color: "#ffffff",
         }}
--- apps/web/src/components/modals/QrCodeModal.tsx
+++ apps/web/src/components/modals/QrCodeModal.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import QrCodeGenerator from '../QrCodeGenerator';
+import React from "react";
+import QrCodeGenerator from "../QrCodeGenerator";
 
 interface QrCodeModalProps {
-    isOpen: boolean;
-    onClose: () => void;
+  isOpen: boolean;
+  onClose: () => void;
 }
 
 const QrCodeModal: React.FC<QrCodeModalProps> = ({ isOpen, onClose }) => {
-    if (!isOpen) return null;
+  if (!isOpen) return null;
 
-    return (
-        <div className="modal modal-open">
-            <div className="modal-box">
-                <QrCodeGenerator data="abcde-fghij-klmno-pqrst" />
-                <div className="modal-action">
-                    <button onClick={onClose} className="btn">Close</button>
-                </div>
-            </div>
+  return (
+    <div className="modal modal-open">
+      <div className="modal-box">
+        <QrCodeGenerator data="abcde-fghij-klmno-pqrst" />
+        <div className="modal-action">
+          <button onClick={onClose} className="btn">
+            Close
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default QrCodeModal;
