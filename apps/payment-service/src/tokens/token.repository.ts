75d4122f547cd2148c53481226a904932c73e9c1
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-07-06 18:14:20 +0800

    Create new token repository methods

--- apps/payment-service/src/tokens/token.repository.ts
+++ apps/payment-service/src/tokens/token.repository.ts
@@ -1,13 +1,23 @@
 import { PrismaClient, Token } from "@repo/payments";
 
 export interface ITokenRepository {
+  findById(id: string): Promise<Token | null>;
   findByAccount(accountId: string): Promise<Array<Token>>;
+  update(token: Token): Promise<Token>;
 }
 
 export default class TokenRepository implements ITokenRepository {
   constructor(private db: PrismaClient) {}
 
+  async findById(id: string): Promise<Token | null> {
+    return this.db.token.findFirst({ where: { id } });
+  }
+
   async findByAccount(accountId: string): Promise<Array<Token>> {
     return this.db.token.findMany({ where: { accountId } });
+  }
+
+  async update(token: Token): Promise<Token> {
+    return this.db.token.update({ where: { id: token.id }, data: token });
   }
 }
