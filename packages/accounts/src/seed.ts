3ab4d71651e49ef5d347ce56a439b7d1f828d364
Tan Jing Sheng <tanjs41200@gmail.com>
2024-07-06 20:01:08 +0800

    Add accounts DB (#25)
    * improve seeding for tokens
    
    * add account db

---
+++ packages/accounts/src/seed.ts
@@ -0,0 +1,34 @@
+import { prisma } from "./client";
+
+import type { Account } from "@repo/accounts";
+
+const DEFAULT_ACCOUNTS = [
+  { id: "clya1jleh00003b6xsw0y6793" },
+  { id: "25r53dknfsler1jj43243989d" },
+  { id:  "1234dsagdadlekelrleklelke"},
+] satisfies Partial<Account>[];
+
+(async () => {
+  try {
+    await Promise.all(
+      DEFAULT_ACCOUNTS.map((account) =>
+        prisma.account.upsert({
+          where: {
+            id: account.id!,
+          },
+          update: {
+            ...account,
+          },
+          create: {
+            ...account,
+          },
+        })
+      ),
+    );
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  } finally {
+    await prisma.$disconnect();
+  }
+})();
