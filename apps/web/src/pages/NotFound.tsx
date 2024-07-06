fb5cb834cb355d7dd68fac0ca4d35e64007ede21
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-07-06 16:03:37 +0800

    Restyle 404 page to tailwind

--- apps/web/src/pages/NotFound.tsx
+++ apps/web/src/pages/NotFound.tsx
@@ -3,12 +3,16 @@ import { useNavigate } from "react-router";
 const NotFound: React.FC = () => {
   const navigate = useNavigate();
   return (
-    <div className="NotFound" style={{ textAlign: "center", margin: "100" }}>
-      <h1>404 Error</h1>
+    <div className="text-center gap-y-3 h-screen flex justify-center items-center flex-col">
+      <h1 className="text-3xl font-extrabold">404 Error</h1>
       <p>Sorry, the page you are looking for cannot be found!</p>
-      <div style={{ display: "inline-block" }}>
-        <button onClick={() => window.history.back()}>Go Back</button>
-        <button onClick={() => navigate("/")}>Home Page</button>
+      <div className="flex gap-x-4 justify-center items-centers mx-auto">
+        <button className="btn" onClick={() => window.history.back()}>
+          Go Back
+        </button>
+        <button className="btn" onClick={() => navigate("/")}>
+          Home Page
+        </button>
       </div>
     </div>
   );
