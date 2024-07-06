8d576663d8d9f4572ed95acd84575f15063988fc
Tan Jing Sheng <tanjs41200@gmail.com>
2024-07-06 21:52:29 +0800

    Add account-service (#29)
    * remove auth-service
    
    * add .env to .gitignore
    
    * update .env.example
    
    * add account-service
    
    * add new line EOF
    
    * fix wrong name

---
+++ apps/account-service/src/internal/config/dependencies.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from "@repo/accounts";
+import { DIContainer } from "rsdi";
+import AccountController from "../../account/account.controller";
+import AccountRepository from "../../account/account.repository";
+import AccountService from "../../account/account.service";
+
+export type AppDIContainer = ReturnType<typeof configureDI>;
+
+export function configureDI() {
+  const container = new DIContainer()
+    // DB Configuration
+    .add("db", () => new PrismaClient())
+    // Tokens
+    .add("accountRepository", ({ db }) => new AccountRepository(db))
+    .add(
+      "accountService",
+      ({ accountRepository }) => new AccountService(accountRepository)
+    )
+    .add(
+      "accountController",
+      ({ accountService }) => new AccountController(accountService)
+    );
+
+  return container;
+}
--- apps/account-service/src/internal/routes/routes.ts
+++ apps/account-service/src/internal/routes/routes.ts
@@ -1,7 +1,7 @@
 import express from "express";
-import * as userRouter from "../../users/user.router";
+import * as accountRouter from "../../account/account.router";
 import { AppDIContainer } from "../config/dependencies";
 
 export const setupRoutes = (app: express.Application, deps: AppDIContainer) => {
-  app.use("/users", userRouter.from(deps.userController));
+  app.use("/account", accountRouter.from(deps.accountController));
 };
