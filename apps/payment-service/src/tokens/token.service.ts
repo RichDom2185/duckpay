f79e941e83b02809ca10ebd256b1ce6b69dee956
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-07-06 18:14:51 +0800

    Update token service to support token consume

--- apps/payment-service/src/tokens/token.service.ts
+++ apps/payment-service/src/tokens/token.service.ts
@@ -2,11 +2,30 @@ import { Token } from "@repo/payments";
 import { ITokenRepository } from "./token.repository";
 
 export interface ITokenService {
+  registerToken(
+    tokenId: string,
+    accountId: string
+  ): Promise<Array<Token> | null>;
   getAllTokensForUser(accountId: string): Promise<Array<Token> | null>;
 }
 
 export default class TokenService implements ITokenService {
   constructor(private tokenRepository: ITokenRepository) {}
+
+  async registerToken(
+    tokenId: string,
+    accountId: string
+  ): Promise<Array<Token> | null> {
+    const token = await this.tokenRepository.findById(tokenId);
+    if (!token) {
+      throw new Error("Token not found");
+    }
+
+    token.accountId = accountId;
+    this.tokenRepository.update(token);
+
+    return this.getAllTokensForUser(accountId);
+  }
 
   async getAllTokensForUser(accountId: string): Promise<Array<Token> | null> {
     // TODO: Check user exists
