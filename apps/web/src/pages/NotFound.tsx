00599431ffaef377e7f04bd604e4b786f4ef42e9
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-07-06 15:58:44 +0800

    Use react-router for routing

--- apps/web/src/pages/NotFound.tsx
+++ apps/web/src/pages/NotFound.tsx
@@ -1,11 +1,14 @@
+import { useNavigate } from "react-router";
+
 const NotFound: React.FC = () => {
+  const navigate = useNavigate();
   return (
-    <div className="NotFound" style={{ textAlign: 'center', margin: '100' }}>
+    <div className="NotFound" style={{ textAlign: "center", margin: "100" }}>
       <h1>404 Error</h1>
       <p>Sorry, the page you are looking for cannot be found!</p>
-      <div style={{ display: 'inline-block' }}>
+      <div style={{ display: "inline-block" }}>
         <button onClick={() => window.history.back()}>Go Back</button>
-        <button onClick={() => window.location.href = '/'}>Home Page</button>
+        <button onClick={() => navigate("/")}>Home Page</button>
       </div>
     </div>
   );
