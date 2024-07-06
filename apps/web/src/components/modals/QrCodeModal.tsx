001f7ba926b3903048f7fae7acf2ce6fd03feeb3
Donovan Singh <don9617@hotmail.com>
2024-07-06 19:28:04 +0800

    Add sweet alert after scan

--- apps/web/src/components/modals/QrCodeModal.tsx
+++ apps/web/src/components/modals/QrCodeModal.tsx
@@ -2,22 +2,26 @@ import React from "react";
 import QrCodeGenerator from "../QrCodeGenerator";
 
 interface QrCodeModalProps {
+  accountId: string;
   tokenId: string;
   isOpen: boolean;
   onClose: () => void;
 }
 
 const QrCodeModal: React.FC<QrCodeModalProps> = ({
+  accountId,
   tokenId,
   isOpen,
   onClose,
 }) => {
   if (!isOpen) return null;
 
+  const qrData = accountId + ":" + tokenId;
+
   return (
     <div className="modal modal-open">
       <div className="modal-box">
-        <QrCodeGenerator data={tokenId} />
+        <QrCodeGenerator data={qrData} />
         <div className="modal-action">
           <button onClick={onClose} className="btn">
             Close
