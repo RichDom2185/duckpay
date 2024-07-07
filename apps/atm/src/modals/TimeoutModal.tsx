5e59254b2d888cb3468f1fcdda45b2f3f4a129a7
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-07-08 01:55:47 +0800

    Create timeout modal

---
+++ apps/atm/src/modals/TimeoutModal.tsx
@@ -0,0 +1,57 @@
+import React, { useCallback, useEffect, useState } from "react";
+import Modal from "../components/common/modals/Modal";
+import { SessionActions } from "../redux/slices/sessionSlice";
+import { useAppDispatch, useAppSelector } from "../redux/store";
+
+const TIMEOUT_SECONDS = 15;
+
+const TimeoutModal: React.FC = () => {
+  const dispatch = useAppDispatch();
+
+  const handleTimeout = useCallback(() => {
+    dispatch(SessionActions.setTimeout(true));
+  }, [dispatch]);
+  const handleTimeoutCancel = useCallback(() => {
+    dispatch(SessionActions.setTimeout(false));
+  }, [dispatch]);
+
+  const isTimeout = useAppSelector((state) => state.session.isTimeout);
+
+  useEffect(() => {
+    const timeout = setTimeout(handleTimeout, TIMEOUT_SECONDS * 1000);
+    return () => clearTimeout(timeout);
+  }, [handleTimeout]);
+
+  const [timerState, setTimerState] = useState(TIMEOUT_SECONDS);
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimerState((prev) => prev - 1);
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <Modal
+      isOpen={isTimeout === undefined}
+      onClickOutside={handleTimeoutCancel}
+      spacing="space-y-4"
+    >
+      <h2 className="text-2xl font-bold">Are you still there?</h2>
+      <p className="text-lg">
+        Your session will be cleared in {timerState} second
+        {timerState !== 1 ? "s" : ""} due to inactivity.
+      </p>
+      <div className="modal-action">
+        <button className="btn btn-primary" onClick={handleTimeoutCancel}>
+          I'm still here
+        </button>
+        <button className="btn btn-ghost" onClick={handleTimeout}>
+          Back to home
+        </button>
+      </div>
+    </Modal>
+  );
+};
+
+export default TimeoutModal;
