beb8e4bdd3389a5655611f88a024792c4eaa970c
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-07-08 03:45:31 +0800

    Design deposit page

--- apps/atm/src/pages/Deposit.tsx
+++ apps/atm/src/pages/Deposit.tsx
@@ -1,7 +1,30 @@
+import { Icon } from "@iconify/react";
 import React from "react";
+import duckpay from "../../assets/duck-transparent-bg.png";
+import Page from "../components/common/Page";
+
+const spacer = <div className="h-14" />;
 
 const Deposit: React.FC = () => {
-  return <div>Deposit</div>;
+  return (
+    <Page>
+      <div className="text-3xl flex flex-col gap-y-8 items-center justify-between h-full">
+        {spacer}
+        <img className="h-64" src={duckpay} alt="DuckPay Logo" />
+        <h3 className="font-semibold tracking-wide">
+          Please enter cash below:
+        </h3>
+        <button className="btn btn-primary btn-block btn-lg text-3xl">
+          Done
+        </button>
+        {spacer}
+        <Icon
+          className="animate-bounce text-9xl"
+          icon="tabler:arrow-big-down-lines"
+        />
+      </div>
+    </Page>
+  );
 };
 
 export const Component = Deposit;
