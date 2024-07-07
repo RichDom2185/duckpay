a6e92a76da897b55f53440ba0bd193a9a112185c
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-07-07 16:36:47 +0800

    Migrate `QrCodeModal` to use new modal

--- apps/web/src/components/modals/QrCodeModal.tsx
+++ apps/web/src/components/modals/QrCodeModal.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import QrCodeGenerator from "../QrCodeGenerator";
 import { Token } from "../../types/types";
 import TokenValueLabel from "../common/labels/TokenValueLabel";
+import Modal from "../common/modals/Modal";
+import QrCodeGenerator from "../QrCodeGenerator";
 
 interface QrCodeModalProps {
   token: Token;
@@ -14,25 +15,21 @@ const QrCodeModal: React.FC<QrCodeModalProps> = ({
   isOpen,
   onClose
 }) => {
-  if (!isOpen) return null;
-
   const qrData = token.accountId + ":" + token.id;
 
   return (
-    <div className="modal modal-open">
-      <div className="modal-box">
-        <p className="pb-4 text-xl text-center">
-          Scan this QR code to <i>Duckpay</i> with this token.
-        </p>
-        <QrCodeGenerator data={qrData} />
-        <TokenValueLabel amount={token.amount} currency={token.currency} />
-        <div className="modal-action">
-          <button onClick={onClose} className="btn">
-            Close
-          </button>
-        </div>
+    <Modal isOpen={isOpen} onClickOutside={onClose}>
+      <p className="pb-4 text-xl text-center">
+        Scan this QR code to <i>Duckpay</i> with this token.
+      </p>
+      <QrCodeGenerator data={qrData} />
+      <TokenValueLabel amount={token.amount} currency={token.currency} />
+      <div className="modal-action">
+        <button onClick={onClose} className="btn">
+          Close
+        </button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
