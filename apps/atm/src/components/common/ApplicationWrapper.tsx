d251dc102073ea18a7372253133f8b40ba188b82
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-07-08 01:55:56 +0800

    Show timeout modal app-wide

--- apps/atm/src/components/common/ApplicationWrapper.tsx
+++ apps/atm/src/components/common/ApplicationWrapper.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Toaster } from "react-hot-toast";
 import { Outlet } from "react-router";
 import { useThemeLoader } from "../../hooks/useThemeLoader";
+import TimeoutModal from "../../modals/TimeoutModal";
 import Toast from "./Toast";
 
 const ApplicationWrapper: React.FC = () => {
@@ -11,6 +12,7 @@ const ApplicationWrapper: React.FC = () => {
     <>
       <Outlet />
       <Toaster>{(t) => <Toast toast={t} />}</Toaster>
+      <TimeoutModal />
     </>
   );
 };
