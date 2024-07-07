70115cc388d3ee9d8b940feb7fa64df5d02cd160
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-07-07 15:15:52 +0800

    Fix local service discoverability

--- apps/payment-service/src/main.ts
+++ apps/payment-service/src/main.ts
@@ -1,7 +1,7 @@
 import app from "./app";
 
 const PORT = parseInt(process.env.PORT || "8001");
-const HOST = process.env.HOST || "localhost";
+const HOST = process.env.HOST || "0.0.0.0";
 
 app.listen(PORT, HOST, () => {
   console.log(`Server is running on http://${HOST}:${PORT}`);
