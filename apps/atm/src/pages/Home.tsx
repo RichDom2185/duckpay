e394ffefa2931816d21e31e44777609bd29b7e6a
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-07-08 01:09:34 +0800

    Scaffold basic layout

--- apps/atm/src/pages/Home.tsx
+++ apps/atm/src/pages/Home.tsx
@@ -1,8 +1,34 @@
+import clsx from "clsx";
 import React from "react";
 import Menu from "../components/Menu";
+import Card from "../components/common/Card";
 
 const Home: React.FC = () => {
-  return <Menu />;
+  return (
+    <>
+      <Menu />
+      <div className="flex gap-x-6 justify-center items-stretch h-[30vh] select-none">
+        <Card
+          bodyClassName={clsx(
+            "justify-center",
+            "font-semibold text-4xl",
+            "tracking-wider"
+          )}
+        >
+          Deposit
+        </Card>
+        <Card
+          bodyClassName={clsx(
+            "justify-center",
+            "font-semibold text-4xl",
+            "tracking-wider"
+          )}
+        >
+          Withdraw
+        </Card>
+      </div>
+    </>
+  );
 };
 
 export const Component = Home;
