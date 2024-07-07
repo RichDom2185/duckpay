a7a0bdabb5623c8090452aeb6fa0d566d7e89e80
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-07-07 17:05:48 +0800

    Remove test tokens data
    Also fixed compile errors.

--- apps/web/src/pages/Home.tsx
+++ apps/web/src/pages/Home.tsx
@@ -1,101 +1,15 @@
+import React from "react";
 import Menu from "../components/Menu";
 import TokenCard from "../components/TokenCard";
-import { Token } from "../types/types";
-import { useState } from "react";
-
-const testTokens: Token[] = [
-  {
-    id: "id1sbdkjabskfjdbsaljbfaj3025uy8913u498h9hw3hr9qhsbljasbaslbflsdbfguhse",
-    accountId: "test",
-    amount: 10,
-    currency: "SGD",
-    createdAt: new Date("2023-01-01"),
-    deletedAt: null
-  },
-  {
-    id: "id28y5-y34qv8nu38fq58n3fqd8m-f85u-dv5yn98w4ngy5894yfytb40qq3dnq-fn-qnv",
-    accountId: "test",
-    amount: 20,
-    currency: "SGD",
-    createdAt: new Date("2023-02-01"),
-    deletedAt: null
-  },
-  {
-    id: "id346729y69384yng-98qef84vng-w5awntpiehpqn-g49fuN{NUy53g(gy35w938ny5o",
-    accountId: "test",
-    amount: 30,
-    currency: "SGD",
-    createdAt: new Date("2023-03-01"),
-    deletedAt: null
-  },
-  {
-    id: "id4viert9-enuqg-948r84NY9W5YGNYG46yy8wre8ur-43uqm9utpnwrhpRNPIwnignp",
-    accountId: "test",
-    amount: 40,
-    currency: "SGD",
-    createdAt: new Date("2023-04-01"),
-    deletedAt: null
-  },
-  {
-    id: "id5fjvinrt895gq-u6qub4tnuesdamievnifu[rfNU%EUGNVMRNBe9mfwv{[vW3U905[",
-    accountId: "test",
-    amount: 50,
-    currency: "SGD",
-    createdAt: new Date("2023-05-01"),
-    deletedAt: null
-  },
-  {
-    id: "id6HVP#8QhrhcHVCHERHEWHw35VRPX9AWVeEMCSXRUE9Uuepvtaebtebyhapcvmpapvp",
-    accountId: "test",
-    amount: 60,
-    currency: "SGD",
-    createdAt: new Date("2023-06-01"),
-    deletedAt: null
-  },
-  {
-    id: "id7u4tnv3fqnP8N5U38-UMJVNTTM--MVR-mtvwmqvtmquvmaw-a5894u31-53v914=1",
-    accountId: "test",
-    amount: 70,
-    currency: "SGD",
-    createdAt: new Date("2023-07-01"),
-    deletedAt: null
-  },
-  {
-    id: "id8vn-t4uv5y4yu=u-2ud9skuduf4f--U4F5DQ2MV4UTN45uvu4ww5ftu8138v4-cm",
-    accountId: "test",
-    amount: 80,
-    currency: "SGD",
-    createdAt: new Date("2023-08-01"),
-    deletedAt: null
-  },
-  {
-    id: "id90anev0tryq0PNYV83tMVR8Rtnv402vtmwerc88rb-wftueu9jmfosiahhyeuy08",
-    accountId: "test",
-    amount: 90,
-    currency: "SGD",
-    createdAt: new Date("2023-09-01"),
-    deletedAt: null
-  },
-  {
-    id: "id10t2h89y56-12hncrjmP8UP4933QTu9-U842Y4HTHPVN9W1-M493U2Mmr9uteryp",
-    accountId: "test",
-    amount: 100,
-    currency: "SGD",
-    createdAt: new Date("2023-10-01"),
-    deletedAt: null
-  }
-];
+import { useAppSelector } from "../redux/store";
 
 const Home: React.FC = () => {
-  const [tokens, setTokens] = useState<Token[]>(testTokens);
-
+  const tokens = useAppSelector((state) => state.session.tokens);
   return (
     <>
-      <Menu tokens={tokens} setTokens={setTokens} />
+      <Menu />
       <div className="flex flex-wrap justify-center items-center m-4">
-        {tokens.map((token) => (
-          <TokenCard key={token.id} token={token} />
-        ))}
+        {tokens?.map((token) => <TokenCard key={token.id} token={token} />)}
       </div>
     </>
   );
