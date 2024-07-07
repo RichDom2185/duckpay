8a96c360e9835a224ed2ca33e1a3c33c5aec31e1
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-07-08 03:47:14 +0800

    Create more pages

---
+++ apps/atm/src/pages/Done.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Page from "../components/common/Page";
+
+const Done: React.FC = () => {
+  return <Page>Done</Page>;
+};
+
+export const Component = Done;
+Component.displayName = "Done";
+
+export default Done;
--- apps/atm/src/routes/routes.ts
+++ apps/atm/src/routes/routes.ts
@@ -4,6 +4,8 @@ const ApplicationWrapper = () =>
   import("../components/common/ApplicationWrapper");
 const Home = () => import("../pages/Home");
 const Deposit = () => import("../../../atm/src/pages/Deposit");
+const Claim = () => import("../../../atm/src/pages/Claim");
+const Done = () => import("../../../atm/src/pages/Done");
 const Withdraw = () => import("../../../atm/src/pages/Withdraw");
 const NotFound = () => import("../pages/NotFound");
 
@@ -14,13 +16,17 @@ export const routes: RouteObject[] = [
     children: [
       { path: "", lazy: Home },
       { path: "deposit", lazy: Deposit },
+      { path: "claim", lazy: Claim },
+      { path: "done", lazy: Done },
       { path: "withdraw", lazy: Withdraw },
       { path: "*", lazy: NotFound }
     ]
   }
 ];
 
-type RouteKeys = Uppercase<"Home" | "Deposit" | "Withdraw" | "NotFound">;
+type RouteKeys = Uppercase<
+  "Home" | "Deposit" | "Claim" | "Done" | "Withdraw" | "NotFound"
+>;
 const buildRouteNames = (routes: RouteObject[], prefix = "") => {
   return routes.reduce(
     (acc, route) => {
