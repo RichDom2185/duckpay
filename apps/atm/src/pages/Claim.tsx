3b8fd99de55332075ca2771597ae4c471d3108e5
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-07-08 04:18:29 +0800

    Setup ATM 4 (#58)
    * Design deposit page
    
    * Create more pages
    
    * Fix route names bug
    
    * Remove unused component
    
    * Add QR scanner package
    
    * Create withdraw page
    
    * Wire up deposit page to claim page

---
+++ apps/atm/src/pages/Claim.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Page from "../components/common/Page";
+
+const Claim: React.FC = () => {
+  return <Page>Claim</Page>;
+};
+
+export const Component = Claim;
+Component.displayName = "Claim";
+
+export default Claim;
--- apps/atm/src/pages/Deposit.tsx
+++ apps/atm/src/pages/Deposit.tsx
@@ -1,7 +1,36 @@
+import { Icon } from "@iconify/react";
 import React from "react";
+import { useNavigate } from "react-router";
+import duckpay from "../../assets/duck-transparent-bg.png";
+import Page from "../components/common/Page";
+import { RouteNames } from "../routes/routes";
+
+const spacer = <div className="h-14" />;
 
 const Deposit: React.FC = () => {
-  return <div>Deposit</div>;
+  const navigate = useNavigate();
+  return (
+    <Page>
+      <div className="text-3xl flex flex-col gap-y-8 items-center justify-between h-full">
+        {spacer}
+        <img className="h-64" src={duckpay} alt="DuckPay Logo" />
+        <h3 className="font-semibold tracking-wide">
+          Please enter cash below:
+        </h3>
+        <button
+          className="btn btn-primary btn-block btn-lg text-3xl"
+          onClick={() => navigate(RouteNames.CLAIM)}
+        >
+          Done
+        </button>
+        {spacer}
+        <Icon
+          className="animate-bounce text-9xl"
+          icon="tabler:arrow-big-down-lines"
+        />
+      </div>
+    </Page>
+  );
 };
 
 export const Component = Deposit;
