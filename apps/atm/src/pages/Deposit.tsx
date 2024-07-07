4b2d5c0f3e25b52ed32b3be2b1c457cd7eab8e1e
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-07-08 04:08:32 +0800

    Wire up deposit page to claim page

--- apps/atm/src/pages/Deposit.tsx
+++ apps/atm/src/pages/Deposit.tsx
@@ -1,11 +1,14 @@
 import { Icon } from "@iconify/react";
 import React from "react";
+import { useNavigate } from "react-router";
 import duckpay from "../../assets/duck-transparent-bg.png";
 import Page from "../components/common/Page";
+import { RouteNames } from "../routes/routes";
 
 const spacer = <div className="h-14" />;
 
 const Deposit: React.FC = () => {
+  const navigate = useNavigate();
   return (
     <Page>
       <div className="text-3xl flex flex-col gap-y-8 items-center justify-between h-full">
@@ -14,7 +17,10 @@ const Deposit: React.FC = () => {
         <h3 className="font-semibold tracking-wide">
           Please enter cash below:
         </h3>
-        <button className="btn btn-primary btn-block btn-lg text-3xl">
+        <button
+          className="btn btn-primary btn-block btn-lg text-3xl"
+          onClick={() => navigate(RouteNames.CLAIM)}
+        >
           Done
         </button>
         {spacer}
