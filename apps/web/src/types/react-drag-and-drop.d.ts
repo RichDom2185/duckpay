f264eeb1afb11adcc7835231124f892a93f459ce
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-07-07 11:53:19 +0800

    Fix module typing

--- apps/web/src/types/react-drag-and-drop.d.ts
+++ apps/web/src/types/react-drag-and-drop.d.ts
@@ -1,5 +1,1 @@
-// src/types/react-drag-and-drop.d.ts
-declare module "react-drag-and-drop" {
-  export class Draggable extends React.Component<any, any> {}
-  export class Droppable extends React.Component<any, any> {}
-}
+declare module "react-drag-and-drop";
