dcfba0aee94e993f16c7516be6cde0169cc5b4eb
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-07-08 02:42:52 +0800

    Add theme selector to application wrapper

--- apps/atm/src/components/common/ApplicationWrapper.tsx
+++ apps/atm/src/components/common/ApplicationWrapper.tsx
@@ -4,12 +4,14 @@ import { Outlet } from "react-router";
 import { useThemeLoader } from "../../hooks/useThemeLoader";
 import TimeoutModal from "../../modals/TimeoutModal";
 import Toast from "./Toast";
+import ThemeSelector from "./buttons/ThemeSelector";
 
 const ApplicationWrapper: React.FC = () => {
   useThemeLoader();
 
   return (
     <>
+      <ThemeSelector />
       <Outlet />
       <Toaster>{(t) => <Toast toast={t} />}</Toaster>
       <TimeoutModal />
